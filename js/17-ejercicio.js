4b6ea69163debf97326ecca7786eb472ed303097
facundo cajal <facundocajal199@gmail.com>
2023-03-08 00:21:01 -0300

    se agrego el ultimo ejercicio del tp

--- js/17-ejercicio.js
+++ js/17-ejercicio.js
@@ -2,12 +2,20 @@
 17- Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
 */
 
-let texto = prompt("Introduce un texto:");
 
-let primeraVocal = texto.search(/[aeiouAEIOU]/); // Busca la primera vocal en el texto
 
-if (primeraVocal >= 1) { // Si se encontró una vocal
-  alert("La primera vocal está en la posición " + primeraVocal);
-} else { // Si no se encontró ninguna vocal
-  alert("No se encontró ninguna vocal en el texto");
+let texto = prompt("Ingrese un texto:");
+let vocales = "aeiouAEIOU";
+
+// Buscar la primera vocal en el texto
+for (let i = 0; i < texto.length; i++) {
+  if (vocales.indexOf(texto[i]) !== -1) {
+    alert(`La primera vocal en el texto '${texto}' se encuentra en la posición ${i+1}`);
+    break;
+  }
+}
+
+// Si no se encuentra ninguna vocal en el texto
+if (i === texto.length) {
+  alert(`No se encontraron vocales en el texto '${texto}'`);
 }
